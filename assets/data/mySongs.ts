dbb401de8d889dba722f226a8ea93b44b9c3da6b
tinaongster <52726695+tinaongster@users.noreply.github.com>
2019-07-23 19:55:55 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,37 @@
+import { songInfo } from "../../app/songInfo";
+
+export const MYSONGS : songInfo[] = [
+    {
+        songname: "Stronger",
+        artist: "Kanye West",
+        genre: "Hip Hop",
+        yearreleased: 2007,
+        picture: "assets/images/Graduation.jpg",
+    },
+
+    {
+        songname: "I'm Upset",
+        artist: "Drake",
+        genre: "Hip Hop",
+        yearreleased: 2018,
+        picture: "assets/images/scorpion.jpg"
+    },
+
+    {
+        songname: "What Do You Mean",
+        artist: "Justin Bieber",
+        genre: "R&B",
+        yearreleased: 2015,
+        picture: "assets/images/purpose.jpg"
+    },
+
+    {
+        songname: "Nervous",
+        artist: "Shawn Mendes",
+        genre: "Pop",
+        yearreleased: 2018,
+        picture: "assets/images/shawn.jpg"
+    }
+
+]
+
